342b94f917f221072aebba40d84ffa376ceab6d8
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-06 23:56:16 -0300

    First button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Button variant="contained">Is this a f***ing button?</Button>
+      </header>
     </div>
   );
 }
