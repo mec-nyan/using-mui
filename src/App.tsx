a4b8c85f0595f572790b8df564cac75ae8ff14a9
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-07 00:03:08 -0300

    hi!

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,34 @@ import "./App.css";
 import { Button } from "@mui/material";
 
 function App() {
+  function sayHi() {
+    alert("Hi!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="contained">Is this a f***ing button?</Button>
-        <Button variant="text">I guess it is...</Button>
-        <Button variant="outlined">Although...</Button>
+        <div>
+          <Button variant="contained">Is this a f***ing button?</Button>
+          <Button variant="text">I guess it is...</Button>
+          <Button variant="outlined">Although...</Button>
+        </div>
+        <div>
+          <Button variant="contained" disabled>
+            Is this a f***ing button?
+          </Button>
+          <Button variant="text" disabled>
+            I guess it is...
+          </Button>
+          <Button variant="outlined" disabled>
+            Although...
+          </Button>
+        </div>
+        <div>
+          <Button variant="contained" onClick={sayHi}>
+            Hi!
+          </Button>
+        </div>
       </header>
     </div>
   );
